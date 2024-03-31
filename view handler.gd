86c8ad045cca79c46866765ea7d841554c6eb532
Damien Jones, Dragonnoot <77818234+Dragonnoot@users.noreply.github.com>
2024-03-30 20:48:02 -0400

    Add a simple background +0+!
    (0) - This engine and IDE is terrible to use I'm sorry
    "How do I do this?" - "Oh no you have to drag and drop
    there isn't anything built into the IDE to let you do
    that lol" I am going to commit a war crime over this.
    
    (!) - Doesn't scale to window resize. Only scales once.
    This program almost made me cry bro I tried for THREE
    FUCKING HOURS to make a RECTANGLE only to find out you
    aren't allowed to do that and you actually just have to
    DRAG AND FUCKING DROP IT ON THE SCREEN, WHICH I KNEW YOU
    COULD DO BUT WANTED ACTUAL FUNCTIONALITY BUT NOOOOOOOOO

--- view handler.gd
+++ view handler.gd
@@ -1,7 +1,28 @@
-extends Node2D;
+extends Area2D;
+
+var viewportPos;
+var scaleChange;
+
+#func updateDisplaySize():
+	#viewportPos = OS.get_window_safe_area().size;
+	#viewportPos.x += 35;
+	#viewportPos.y -= 35;
+	#scaleChange = Vector2(viewportPos.x / $"ui-backingMule".position.x, viewportPos.y / $"ui-backingMule".position.y);
+#
+	##$"ui-backingMule".set_region_rect(Rect2(Vector2(0, 0), viewportPos));
+	#$"ui-backingMule".scale.x = scaleChange[0] * $"ui-backingMule".scale.x;
+	#$"ui-backingMule".scale.y = scaleChange[1] * $"ui-backingMule".scale.y;
+	#$"ui-backingMule".set_flip_v(true);
+	#   KILL ME
+#
+#func unadjust(vec):
+	#return Vector2(vec.x - 35, vec.y + 35);
 
 func _ready():
-	print("Hello World!");
+	#updateDisplaySize();
+	pass;
 
 func _process(delta):
-	pass;
+	#if(get_viewport_rect() != unadjust(viewportPos)):
+		#updateDisplaySize();
+		pass;
